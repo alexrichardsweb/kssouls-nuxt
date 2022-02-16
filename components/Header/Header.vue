2ecdba7fb47f598c716cd3d7638c98683283f1fe
Alex Richards <alexrichards@Alexs-MacBook-Pro.local>
2022-02-16 19:13:33 +0000

    Backend update

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="header bg-black c-white">
-    <div class="header__main p-2">
-      <div class="header__menu c-white">
+  <div class="header bg-black c-white box-shadow">
+    <div class="header__main p-1 max-content-width">
+      <div class="header__logo">
+        Logo
+      </div>
+      <ul class="header__menu c-white ta-right">
         <li
           v-for="item in menuItems"
           :key="item.title"
+          class="tf-allcaps tf-heading tf-bold"
         >
-          {{ item.title }}
+          <a
+            v-if="item.page_type === 'external'"
+            :href="item.link"
+            :title="item.title"
+            target="_blank"
+          >
+            {{ item.title }}
+          </a>
+          <nuxt-link
+            v-else
+            :to="item.page"
+            :title="item.title"
+          >
+            {{ item.title }}
+          </nuxt-link>
         </li>
-      </div>
+      </ul>
     </div>
     <div class="header__checkered bg-checkered" />
   </div>
@@ -31,6 +49,12 @@ export default {
       }
       return [];
     },
+    logo () {
+      if (this.settings) {
+        return this.settings.logo;
+      }
+      return {};
+    },
   },
   mounted () {
   },
@@ -40,11 +64,42 @@ export default {
 <style lang="scss" scoped>
 
 .header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+
+  &__main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__logo {
+    width: 100px;
+  }
 
   &__checkered {
     width: 100%;
-    height: 30px;
+    height: 26px;
   }
+
+  &__menu {
+    display: flex;
+    @include fontSize(20px);
+  }
+}
+
+@include breakpoint('landscape-tablet') {
+
+  .header {
+    &__menu {
+      li {
+        margin-left: 1.5rem;
+      }
+    }
+  }
+
 }
 
 </style>

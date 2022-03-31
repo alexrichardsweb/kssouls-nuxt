f27e88f9b304427cbe535d5ad95f116347c37491
Alex Richards <alexrichards@Alexs-MacBook-Pro.local>
2022-03-31 12:15:36 +0100

    generate fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,12 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import global from '~/mixins.js/global.js';
+import constants from '~/mixins.js/constants.js';
+
+Vue.mixin(global);
+Vue.mixin(constants);
 
 export default {
 };
